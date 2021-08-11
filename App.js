34251fd8d2006a3cd38f151cadcfd0903cd91abc
mrpmohiburrahman <mrpmohiburrahman@gmail.com>
2021-08-11 20:18:53 +0600

    add sample code and data for highcharts

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import HighchartsReactNative from '@highcharts/highcharts-react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            chartOptions: {
+              xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+              },
+              plotOptions: {
+                series: {
+                    events: {
+                      },
+                showInNavigator: true,
+                  }
+              },
+        
+              series: [
+                { data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4] },
+                // { data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4] }
+              ]
+            }
+        };
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <HighchartsReactNative
+                    styles={styles.container}
+                    options={this.state.chartOptions}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        flex: 1
+    }
 });
